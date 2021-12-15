9b82a534a1d4cfb75cc9b0a9c71583786abec0d8
khadetou <khadetou96@gmail.com>
2021-12-15 20:52:04 +0000

    searchRepositories action

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+export const searchRepositories = (term: string) => {
+  return async (dispatch: any) => {
+    dispatch({
+      type: ActionType.SEARCH_REPOSITORIES,
+    });
+    try {
+      const res = axios.get("https://registry.npmjs.org/-/v1/search?text=", {
+        params: {
+          text: term,
+        },
+      });
+      console.log(res);
+      //   const names = res.objects.map((result: any) => {
+      //     return result.package.name;
+      //   });
+      //   dispatch({
+      //     type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+      //     payload: names,
+      //   });
+    } catch (error: any) {
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_ERROR,
+        payload: error.message,
+      });
+    }
+  };
+};
