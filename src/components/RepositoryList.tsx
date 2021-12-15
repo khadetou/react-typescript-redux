88e23aa96c87a2c4310c6a0ffd9a658d6872761f
khadetou <khadetou96@gmail.com>
2021-12-15 21:34:23 +0000

    input sumbmit setting

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const RepositoryList: React.FC = () => {
+    const [term, setTerm] = useState('');
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(term);
+    }
     return (
         <div>
-            <form >
-                <input type="text" />
+            <form onSubmit={onSubmit}>
+                <input type="text" value={term} onChange={(e) => setTerm(e.target.value)} />
                 <button>Search</button>
             </form>
         </div>
